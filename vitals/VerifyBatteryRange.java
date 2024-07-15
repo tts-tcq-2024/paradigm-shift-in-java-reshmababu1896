075d027442e52f72d47b5ca6bdc1379e3c0f3e0a
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-15 10:47:08 +0530

    Update VerifyBatteryRange.java

--- vitals/VerifyBatteryRange.java
+++ vitals/VerifyBatteryRange.java
@@ -2,43 +2,48 @@ package vitals;
 import java.util.logging.Logger;
 
 public class VerifyBatteryRange {
-     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return temperatureIsOk(temperature) && stateIsOk(soc) && chargeIsOk(chargeRate);
+     
+     static boolean verifyBatteryIsOk(float temperature, float soc, float chargeRate) {
+        return validateTemperatureIsOk(temperature) && validateStateIsOk(soc) && validateChargeIsOk(chargeRate);
     }
 
-    static boolean temperatureIsOk(float temperature) {
+    static boolean validateTemperatureIsOk(float temperature) {
         boolean validValue=true;
         if(temperature < 0 || temperature > 45){
-            System.out.println("Temperature out of range");
+            printValueOutOfRangeMessage("Temperature out of range");
             validValue=false;
         }
         return validValue;
     }
 
-    static boolean stateIsOk(float soc) {
+    static boolean validateStateIsOk(float soc) {
         boolean validValue=true;
         if(soc < 20 || soc > 80){
-            System.out.println("State of charge is out of range");
+            printValueOutOfRangeMessage("State of charge is out of range");
             validValue=false;
         }
         return validValue;
     }
 
-    static boolean chargeIsOk(float chargeRate) {
+    static boolean validateChargeIsOk(float chargeRate) {
         boolean validValue=true;
         if(chargeRate>0.8f){
-            System.out.println("Charge of rate is out of range");
+           printValueOutOfRangeMessage("Charge of rate is out of range");
             validValue=false;
         }
         return validValue;
     }
+     
+     static void printValueOutOfRangeMessage(String message){
+          System.out.println(message);
+     }
   
     public static void main(String[] args) {
-        assert (batteryIsOk(25, 70, 0.7f));
-        assert (batteryIsOk(0, 20, 0.0f));
-        assert (batteryIsOk(45, 80, 0.8f));
+        assert (verifyBatteryIsOk(25, 70, 0.7f));
+        assert (verifyBatteryIsOk(0, 20, 0.0f));
+        assert (verifyBatteryIsOk(45, 80, 0.8f));
       
-        assert (!batteryIsOk(25, 10, 0.7f));
-        assert (!batteryIsOk(25, 70, 0.9f));
+        assert (!verifyBatteryIsOk(25, 10, 0.7f));
+        assert (!verifyBatteryIsOk(25, 70, 0.9f));
     }
 }
