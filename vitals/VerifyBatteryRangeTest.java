dc9374d2228feab175bbf68a44716333cc190333
reshmababu1896 <reshmababu1896@gmail.com>
2024-08-26 10:32:52 +0530

    Update VerifyBatteryRangeTest.java

--- vitals/VerifyBatteryRangeTest.java
+++ vitals/VerifyBatteryRangeTest.java
@@ -4,9 +4,15 @@ public class VerifyBatteryRangeTest {
     VerifyBatteryRange verifyBatteryRange=new VerifyBatteryRange();
 
     public void testBatteryIsOk(){
-        assert (verifyBatteryRange.verifyBatteryIsOk(25, 70, 0.7f));
+        assert (verifyBatteryRange.verifyBatteryIsOk(25, 50, 0.7f));
+    }
+
+    public void testLowTempWarning(){
         assert (verifyBatteryRange.verifyBatteryIsOk(0, 20, 0.0f));
-        assert (verifyBatteryRange.verifyBatteryIsOk(45, 80, 0.8f));
+    }
+    
+    public void testHighTempWarning(){
+         assert (verifyBatteryRange.verifyBatteryIsOk(45, 80, 0.8f));
     }
 
     public void testRateOfChargebreach(){
